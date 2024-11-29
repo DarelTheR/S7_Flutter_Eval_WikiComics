03a22c400c7470908b0e63b7410043530a3680ba
DEVRED Paul <devred@et.intechinfo.fr>
2024-11-29 16:26:44 +0100

    colors added

---
+++ lib/res/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color Background = Color(0x15232E);
+  static const Color Section_1E3243 = Color(0x1E3243);
+  static const Color Button = Color(0x000000);
+  static const Color Blue_blue = Color(0x284C6A);
+  static const Color Bottom_bar = Color(0x0F1E2B);
+  static const Color Blue_3792FF = Color(0x3792FF);
+  static const Color Icone = Color(0x778BA8);
+  static const Color Orange = Color(0xFF8100);
+  static const Color White = Color(0xFFFFFF);
+  static const Color Blue_text = Color(0x1F9FFF);
+}
