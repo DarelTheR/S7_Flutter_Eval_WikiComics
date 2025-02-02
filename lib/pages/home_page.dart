2e3e5c3963cc3329c67cf96146108796a8ceb145
areapmaod <lyeslmr0@gmail.com>
2025-02-02 20:59:54 +0100

    modif home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -155,17 +155,26 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
-              GestureDetector(
-                onTap: () {
+              TextButton(
+                onPressed: () {
                   context.push(route);
                 },
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  backgroundColor: AppColors.Background,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
                 child: const Text(
                   "Voir plus",
                   style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontFamily: 'Nunito'),
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Nunito',
+                  ),
                 ),
               ),
             ],
